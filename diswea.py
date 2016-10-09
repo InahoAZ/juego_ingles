6711873a395b02eb90cbbaa59889cf3ccf7a9c89
Santy Villar <Santy Villar>
2016-10-09 20:00:09 -0300

    Ahora el pj pelirrojo se mueve :v

--- diswea.py
+++ diswea.py
@@ -3,27 +3,49 @@ import pilasengine
 pilas = pilasengine.iniciar()
 
 #PERSONAJES
+class Poroncho(pilasengine.actores.Actor):
+
+	def iniciar(self):
+		self.imagen = 'imagenes/personajes/poroncho/standing/frame-1.png'
+            
+    
+                
+
 class Pelirrojo(pilasengine.actores.Actor):
 
 	def iniciar(self):
 		self.animacion_detenida = pilas.imagenes.cargar_grilla("imagenes/personajes/pelirrojo/idle/frame-1.png", 1)
-		self.animacion_movimiento = pilas.imagenes.cargar_grilla("imagenes/personajes/pelirrojo/running/frame-1.png")
+		self.animacion_movimiento = pilas.imagenes.cargar_grilla("imagenes/personajes/pelirrojo/running/grilla.png", 6)
 	
 	def poner_en_movimiento(self):
 		self.imagen = self.animacion_movimiento
 
+		
 	def poner_en_reposo(self):
 		self.imagen = self.animacion_detenida
 
-	def actualizar(self):
-		self.imagen.avanzar()
-
+	def actualizar(self):		
+		self.poner_en_reposo()
 		if pilas.control.izquierda:
+			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.poner_en_movimiento()
 			self.x -= 5
 			self.espejado = True
 		if pilas.control.derecha:
+			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.poner_en_movimiento()
 			self.x += 5 
 			self.espejado = False
+		if pilas.control.abajo:
+			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.poner_en_movimiento()
+			self.y -= 5 
+			self.espejado = False
+		if pilas.control.arriba:
+			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.poner_en_movimiento()
+			self.y += 5 
+			self.espejado = False	
 
 
 
@@ -31,24 +53,29 @@ class Pelirrojo(pilasengine.actores.Actor):
 class ObjetoAleatorio(pilasengine.actores.Actor):
 
 	def iniciar(self):
-		listimg = ["auriculares", "monitor", "blueray", "cooler", "cpu", "impresora", "mic"]
+		listimg = ["auriculares", "blueray", "cooler", "cpu", "hdd", "impresora", "mic", "monitor", "mouse", "pendrive", "teclado", "videocard", "webcam"]
 		q = len(listimg)-1
 		rnd = pilas.azar(0,+q)
 		nameimg = listimg[rnd]
 		self.imagen = "imagenes/"+nameimg+".png"
+		self.x = pilas.azar(-220, 220)
+		self.y = pilas.azar(-280, 280)
 
 
+
+fondo = pilas.fondos.Fondo()
+fondo.imagen = 'imagenes/fondos/tech.png'
 
 
 auriculares = Pelirrojo(pilas)
-auriculares.escala = 0.5
-auriculares.x = -240
+auriculares.escala = 0.2
+auriculares.x = 0
 auriculares.y = 0
 
-blueray = ObjetoAleatorio(pilas)*5
-blueray.escala = 0.1
-blueray.x = pilas.azar(-220,220)
-blueray.y = pilas.azar(-280,280)
+objetos = ObjetoAleatorio(pilas)*4
+objetos.escala = 0.2
+
+
 
 
 
--- mi_juego.py
+++ mi_juego.py
@@ -1,6 +1,10 @@
 import pilasengine
 pilas = pilasengine.iniciar()
 
-impresora = pilas.actores.Actor()
-impresora.imagen = "imagenes/impresora.png"
-impresora.escala = 1
+#impresora = pilas.actores.Actor()
+#impresora.imagen = "imagenes/personajes/pelirrojo/running/grilla.jpg"
+#impresora.escala = 0.2
+
+grilla = pilas.imagenes.cargar_grilla("imagenes/personajes/pelirrojo/running/grilla.png", 6)
+p = grilla.avanzar()
+p.escala = 0.2
