b3285b75d1d62e1d5a3816312e516c8040415ee4
Santy Villar <Santy Villar>
2016-10-10 19:57:53 -0300

    Los objetos se mueven aleaotiamente, para el culo, pero se mueven xd

--- diswea.py
+++ diswea.py
@@ -16,7 +16,9 @@ class Pelirrojo(pilasengine.actores.Actor):
 	def iniciar(self):
 		self.animacion_detenida = pilas.imagenes.cargar_grilla("imagenes/personajes/pelirrojo/idle/grilla.png", 2)
 		self.animacion_movimiento = pilas.imagenes.cargar_grilla("imagenes/personajes/pelirrojo/running/grilla.png", 6)
-	
+		rect = pilas.fisica.Rectangulo(0, 0, 60, 115, sensor=True, dinamica=False)
+		self.figura_de_colision = rect
+		
 	def poner_en_movimiento(self):
 		self.imagen = self.animacion_movimiento
 
@@ -28,22 +30,22 @@ class Pelirrojo(pilasengine.actores.Actor):
 		self.poner_en_reposo()
 		self.animacion_detenida.avanzar(velocidad = 1)
 		if pilas.control.izquierda:
-			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.animacion_movimiento.avanzar(velocidad = 5)
 			self.poner_en_movimiento()
 			self.x -= 5
 			self.espejado = True
 		if pilas.control.derecha:
-			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.animacion_movimiento.avanzar(velocidad = 5)
 			self.poner_en_movimiento()
 			self.x += 5 
 			self.espejado = False
 		if pilas.control.abajo:
-			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.animacion_movimiento.avanzar(velocidad = 5)
 			self.poner_en_movimiento()
 			self.y -= 5 
 			self.espejado = False
 		if pilas.control.arriba:
-			self.animacion_movimiento.avanzar(velocidad = 15)
+			self.animacion_movimiento.avanzar(velocidad = 5)
 			self.poner_en_movimiento()
 			self.y += 5 
 			self.espejado = False	
@@ -60,29 +62,33 @@ class ObjetoAleatorio(pilasengine.actores.Actor):
 		nameimg = listimg[rnd]
 		self.imagen = "imagenes/"+nameimg+".png"
 		self.x = pilas.azar(-220, 220)
-		self.y = pilas.azar(-280, 280)        
+		self.y = pilas.azar(-280, 280)       
 
  	def actualizar(self):
  		a = 0
- 		xrand = pilas.azar(-200, 200)
-		xrand2 = pilas.azar(-200, 200)
-		yrand = pilas.azar(-200, 200)
-		yrand2 = pilas.azar(-200, 200)
-		self.x = [xrand, xrand2],2 
+
+ 	def cambiar_posicion(self):
+ 		xrand = pilas.azar(-400, 400)
+		xrand2 = pilas.azar(-400, 400)
+		yrand = pilas.azar(-400, 400)
+		yrand2 = pilas.azar(-400, 400)
+		self.x = [xrand, xrand2],5
+		self.y = [yrand, yrand2],5 
+		
 
 fondo = pilas.fondos.Fondo()
 fondo.imagen = 'imagenes/fondos/tech.png'
 
 
 auriculares = Pelirrojo(pilas)
-auriculares.escala = 0.2
+auriculares.escala = 0.1
 auriculares.x = 0
 auriculares.y = 0
 
-objetos = ObjetoAleatorio(pilas)*4
+objetos = ObjetoAleatorio(pilas)*8
 objetos.escala = 0.2
 
-
+pilas.tareas.siempre(3, objetos.cambiar_posicion)
 
 
 
---
+++ wa.py
@@ -0,0 +1,17 @@
+xrand = pilas.azar(-200, 200)
+xrand2 = pilas.azar(-200, 200)
+yrand = pilas.azar(-200, 200)
+yrand2 = pilas.azar(-200, 200)
+self.x = [xrand, xrand2] 
+
+
+rect = pilas.fisica.Rectangulo(0, 0, 40, 100, sensor=True, dinamica=False)
+auriculares.figura_de_colision = rect
+
+a = 0
+ 		xrand = pilas.azar(-200, 200)
+		xrand2 = pilas.azar(-200, 200)
+		yrand = pilas.azar(-200, 200)
+		yrand2 = pilas.azar(-200, 200)
+		self.x = [xrand, xrand2],2
+		self.y = [yrand, yrand2],2 
